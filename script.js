64a546e272cc68497d79e592391b276a281903ad
JD-Jaramillo <jaramillo784@gmail.com>
2021-02-23 21:16:22 -0500

    API call works for city and UV index

--- script.js
+++ script.js
@@ -1,59 +1,67 @@
 var appkey = '14c203985535363483dc6e984fdf3c1b';
-// var cityInput = document.getElementById($('#citySearched').val());
-var city = 'charlotte';
-console.log('city');
-// var city = cityInput.value;
+var currentDate = moment().format("MM/D/YYYY");
 var unit = 'imperial';
-var lat = 'response.city.coord.lat';
-var lon = 'response.city.coord.lon';
 
-var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + appkey + '&units=' + unit;
-var uvIndexUrl = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + lon + '&appid=' + appkey;
 
-fetch(weatherUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        var cityEl = document.querySelector('#cityName');
-        console.log(cityEl);
-        cityEl.textContent = city;
-        var tempEl = document.querySelector('#temp');
-        console.log(tempEl);
-        tempEl.textContent = 'Temperature: ' + parseInt(data.main.temp) + '°F';
-        var humidityEl = document.querySelector('#humidity');
-        console.log(humidityEl);
-        humidityEl.textContent = 'Humidity: ' + parseInt(data.main.humidity) + '%';
-        var windEl = document.querySelector('#windspeed');
-        console.log(windEl);
-        windEl.textContent = 'Wind Speed: ' + parseInt(data.wind.speed) + ' MPH';
-        // var uvIndexEl = document.querySelector('#uvindex');
-        // console.log(uvIndexEl);
-        // uvIndexEl.textContent = 'UV Index: ' + parseInt(data.wind.Speed) + 'MPH';
-    })
-fetch(uvIndexUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        var uvIndexEl = document.querySelector('#uvindex');
-        console.log(uvIndexEl);
-        // uvIndexEl.textContent = 'UV Index: ' + parseInt(data.wind.Speed) + 'MPH';
-    })
+function apiCall(x) {
+    var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + x + '&appid=' + appkey + '&units=' + unit;
+    fetch(weatherUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var cityEl = document.querySelector('#cityName');
+            console.log(cityEl);
+            cityEl.textContent = x + ' (' + (currentDate) + ')';
+            var tempEl = document.querySelector('#temp');
+            console.log(tempEl);
+            tempEl.textContent = 'Temperature: ' + parseInt(data.main.temp) + '°F';
+            var humidityEl = document.querySelector('#humidity');
+            console.log(humidityEl);
+            humidityEl.textContent = 'Humidity: ' + parseInt(data.main.humidity) + '%';
+            var windEl = document.querySelector('#windspeed');
+            console.log(windEl);
+            windEl.textContent = 'Wind Speed: ' + parseInt(data.wind.speed) + ' MPH';
+            var lat = parseInt(data.coord.lat);
+            console.log(lat, 'lat');
+            var lon = parseInt(data.coord.lon);
+            console.log(lon, 'lon');
+            var uvIndexUrl = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + lon + '&appid=' + appkey;
 
+            fetch(uvIndexUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                    var uvIndexEl = document.querySelector('#uvindex');
+                    console.log(uvIndexEl);
+                    uvIndexEl.textContent = 'UV Index: ' + parseInt(data.value);
+                })
+
+        })
+
+}
 
 
 var cities = [];
 
 
 $("#searchBtn").on("click", function () {
+    // capturture the value in citySearched
+    var cityInput = $('#citySearched').val().trim();
+    console.log(cityInput, 'cities');
     if (cityInput !== "") {
+
+
         console.log('city button working');
-        var ul = document.querySelector('cityList');
+        var ul = document.querySelector('.cityList');
         var li1 = document.createElement('li');
+        li1.classList.add('cityList__item');
         li1.textContent = cityInput;
         ul.appendChild(li1);
+
+        apiCall(cityInput);
     }
 })
