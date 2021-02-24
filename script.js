41ce8a2bf78be85b6626badfa08c03df0e5e733a
JD-Jaramillo <jaramillo784@gmail.com>
2021-02-23 23:21:34 -0500

    saving up to forecast info

--- script.js
+++ script.js
@@ -3,30 +3,24 @@ var currentDate = moment().format("MM/D/YYYY");
 var unit = 'imperial';
 
 
-function apiCall(x) {
-    var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + x + '&appid=' + appkey + '&units=' + unit;
+function apiCall(cityInput) {
+    var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInput + '&appid=' + appkey + '&units=' + unit;
     fetch(weatherUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
             var cityEl = document.querySelector('#cityName');
-            console.log(cityEl);
-            cityEl.textContent = x + ' (' + (currentDate) + ')';
+            cityEl.textContent = cityInput + ' (' + (currentDate) + ')';
             var tempEl = document.querySelector('#temp');
-            console.log(tempEl);
             tempEl.textContent = 'Temperature: ' + parseInt(data.main.temp) + '°F';
             var humidityEl = document.querySelector('#humidity');
-            console.log(humidityEl);
             humidityEl.textContent = 'Humidity: ' + parseInt(data.main.humidity) + '%';
             var windEl = document.querySelector('#windspeed');
-            console.log(windEl);
             windEl.textContent = 'Wind Speed: ' + parseInt(data.wind.speed) + ' MPH';
             var lat = parseInt(data.coord.lat);
-            console.log(lat, 'lat');
             var lon = parseInt(data.coord.lon);
-            console.log(lon, 'lon');
+
             var uvIndexUrl = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + lon + '&appid=' + appkey;
 
             fetch(uvIndexUrl)
@@ -34,11 +28,8 @@ function apiCall(x) {
                     return response.json();
                 })
                 .then(function (data) {
-                    console.log(data);
                     uvIndexNumber = parseInt(data.value);
-                    console.log('unIndexNumber', uvIndexNumber);
                     var uvIndexEl = document.querySelector('#uvindex');
-                    console.log(uvIndexEl);
                     uvIndexEl.textContent = 'UV Index: ' + uvIndexNumber;
                     if (uvIndexNumber < 4) {
                         uvIndexEl.classList.add('favorable');
@@ -47,13 +38,31 @@ function apiCall(x) {
                     } else {
                         uvIndexEl.classList.add('severe');
                     }
-
                 })
+            var fiveDayAPI = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=' + appkey + '&units=' + unit;
 
+            fetch(fiveDayAPI)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                    var date = '10/21';
+                    var icon = 'this'
+                    var temp = 80;
+                    var humidity = 80;
+                    var forecastWrapper = document.getElementById('forecast__wrapper');
+                    console.log('forecastWrapper', forecastWrapper);
+                    forecastWrapper.innerHTML =
+                        `<div class="forecast__box">
+                    <p class="forecast__date">${date}</p>
+                    <p class="forecast__icon">${icon}</p>
+                    <p class="forecast__temp">Temp: ${temp} F</p>
+                    <p class="forecast__humidity">Humidity: ${humidity}%</p>
+                    </div>`
+                })
         })
-
 }
-
 
 var cities = [];
 
@@ -63,15 +72,14 @@ $("#searchBtn").on("click", function () {
     var cityInput = $('#citySearched').val().trim();
     console.log(cityInput, 'cities');
     if (cityInput !== "") {
-
-
         console.log('city button working');
         var ul = document.querySelector('.cityList');
         var li1 = document.createElement('li');
         li1.classList.add('cityList__item');
         li1.textContent = cityInput;
         ul.appendChild(li1);
+        localStorage.setItem('city', li1)
 
         apiCall(cityInput);
     }
-})
+});
