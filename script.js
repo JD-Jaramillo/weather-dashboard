118e74d878011f4f0856294f48a9596dba394cd4
JD-Jaramillo <jaramillo784@gmail.com>
2021-02-24 21:23:06 -0500

    leading up to local storage working

--- script.js
+++ script.js
@@ -39,55 +39,92 @@ function apiCall(cityInput) {
                         uvIndexEl.classList.add('severe');
                     }
                 })
-            var fiveDayAPI = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=' + appkey + '&units=' + unit;
-
-            fetch(fiveDayAPI)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (data) {
-                    console.log(data);
-                    var data = data;
-                    var arrayList = data.list;
-                    console.log('arrayList', arrayList);
-
-                    arrayList.forEach((array) => {
-                        var noon = array.dt_txt.includes('12:00:00');
-                        console.log('noon', noon);
-                        if (noon) {
-                            console.log('array', array);
-                            var date = array.dt_txt.split(' ')[0];
-                            var temp = array.main.temp;
-                            var humidity = array.main.humidity;
-                            var icon = array.weather[0].icon;
-                            var iconUrl = 'http://openweathermap.org/img/wn/' + icon + '@2x.png'
-                            var forecast = document.querySelector('.forecast__wrapper');
-                            var dayForecastData = document.createElement("div");
-                            dayForecastData.className = 'forecast__box';
-                            dayForecastData.innerHTML = `<p class="forecast__date">${date}</p> <img src="${iconUrl}"> <p class="forecast__temp">Temp: ${temp} F</p> <p class="forecast__humidity">Humidity: ${humidity}%</p>`;
-                            forecast.appendChild(dayForecastData);
-                        }
-                    });
-                })
+            fiveDayAPICall(cityInput);
         })
 }
 
-var cities = [];
+function fiveDayAPICall(cityInput) {
+    var fiveDayAPI = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=' + appkey + '&units=' + unit;
 
+    fetch(fiveDayAPI)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var data = data;
+            var arrayList = data.list;
+            console.log('arrayList', arrayList);
+
+            arrayList.forEach((dayObject) => {
+                var noon = dayObject.dt_txt.includes('12:00:00');
+                if (noon) {
+                    var date = dayObject.dt_txt.split(' ')[0];
+                    var temp = dayObject.main.temp;
+                    var humidity = dayObject.main.humidity;
+                    var icon = dayObject.weather[0].icon;
+                    var iconUrl = 'http://openweathermap.org/img/wn/' + icon + '@2x.png'
+                    var forecast = document.querySelector('.forecast__wrapper');
+                    var dayForecastData = document.createElement("div");
+                    dayForecastData.className = 'forecast__box';
+                    dayForecastData.innerHTML = `<p class="forecast__date">${date}</p> <img src="${iconUrl}"> <p class="forecast__temp">Temp: ${temp} F</p> <p class="forecast__humidity">Humidity: ${humidity}%</p>`;
+                    forecast.appendChild(dayForecastData);
+                }
+            });
+        })
+
+}
+
+function clear5Day() {
+    var forecast = document.querySelector('.forecast__wrapper');
+    forecast.innerHTML = '';
+}
+
+$("#clear_btn").on("click", function () {
+    citiesList = [];
+})
+
+
+// function storingCityInput(cityInput) {
+//     var city = cityInput;
+//     for (var i = 0; i <= 6; i++) {
+//         if (city !== "") {
+//             localStorage.setItem('cityInput', cityInput[i]);
+//             console.log('storage is working', localStorage);
+//         }
+//     }
+// }
+
+var citiesList = localStorage.getItem('cities') || [];
+console.log('cities', citiesList);
+var citiesListUl = document.querySelector('#citiesList');
+console.log(citiesListUl, 'citiesListUl');
 
 $("#searchBtn").on("click", function () {
-    // capturture the value in citySearched
-    var cityInput = $('#citySearched').val().trim();
+    // capture the value in citySearched and trim it to get rid of white spaces
+    var cityInput = $('#citySearched').val().toUpperCase().trim();
     console.log(cityInput, 'cities');
+    // IF value of cityInput is not equal to "" then create an Li element, add a classs, add the city input and append it to the ul
     if (cityInput !== "") {
         console.log('city button working');
-        var ul = document.querySelector('.cityList');
-        var li1 = document.createElement('li');
-        li1.classList.add('cityList__item');
-        li1.textContent = cityInput;
-        ul.appendChild(li1);
-        localStorage.setItem('city', li1)
+        citiesListUl.innerHTML = ' ';
 
+        citiesList.push(cityInput);
+        localStorage.setItem('citiesList', citiesList);
+        console.log('storage', localStorage);
+
+        citiesList.forEach((city) => {
+            var li = document.createElement('li');
+            li.classList.add('cityList__item');
+            li.innerHTML = city;
+            citiesListUl.appendChild(li);
+        });
+
+
+
+
+        clear5Day();
+        // CityInput(cityInput)
         apiCall(cityInput);
     }
 });
